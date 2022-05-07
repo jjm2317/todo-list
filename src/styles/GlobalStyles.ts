1527ab0c00dbafea31cd66e5e1eb150cf628e175
jjm2317 <wlaks2317@gmail.com>
2022-05-07 11:56:56 +0900

    feat: set box-sizing to border-box

--- src/styles/GlobalStyles.ts
+++ src/styles/GlobalStyles.ts
@@ -2,6 +2,12 @@ import { createGlobalStyle } from 'styled-components';
 import reset from 'styled-reset';
 
 const GlobalStyles = createGlobalStyle`
+    html {
+    box-sizing: border-box;
+    }
+    *, *:before, *:after {
+    box-sizing: inherit;
+    }
     ${reset};
 `;
 
